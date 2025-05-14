9376f3a7e7d92c8c030ffa018ad11ff02ba9cc89
mnsuhairil <mnsuhairil@gmail.com>
2025-05-14 14:32:10 +0800

    feat: improve ui design and implement firebase to home section

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,37 +2,43 @@ import { Button, Container, Grid, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import theme from "../themes/theme";
 import suimage from "./../assets/su-img2.png";
+import { getDatabase, ref, onValue } from "firebase/database";
+import app from "../firebase";
 
 interface HomePageProps {
   themeMode: "dark" | "light";
 }
 
 const HomePage: React.FC<HomePageProps> = ({ themeMode }) => {
-  const [typedText, setTypedText] = useState(""); // State for typing animation
-  const [hasAnimated, setHasAnimated] = useState(false); // State to track if animation has run
-  const fullText = `Hello, I'm Muhammad Nur Suhairil. I'm a passionate web developer
-with expertise in front-end and back-end technologies. This is my
-portfolio where I showcase my projects and share a little about
-myself. From creating responsive and user-friendly interfaces to
-implementing robust server-side logic, I thrive on turning ideas
-into functional and aesthetically pleasing websites. Explore my work
-to get a glimpse of my skills and the projects I've brought to life.`;
+  const [typedText, setTypedText] = useState("");
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const [aboutMe, setAboutMe] = useState<string>("");
 
   useEffect(() => {
-    if (!hasAnimated) {
+    const db = getDatabase(app);
+    const aboutMeRef = ref(db, "personalAppDatabase/portfolio_web_data/Biodata/about_me");
+    onValue(aboutMeRef, (snapshot) => {
+      const value = snapshot.val();
+      setAboutMe("  " + value || "");
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!hasAnimated && aboutMe) {
       let index = 0;
+      setTypedText(""); 
       const typingInterval = setInterval(() => {
-        if (index < fullText.length - 1) {
-          setTypedText((prev) => prev + fullText[index]);
+        if (index < aboutMe.length -1) {
+          setTypedText((prev) => prev + aboutMe[index]);
           index++;
         } else {
-          clearInterval(typingInterval); // Stop the interval when typing is complete
-          setHasAnimated(true); // Mark animation as completed
+          clearInterval(typingInterval);
+          setHasAnimated(true);
         }
-      }, 20); // Adjust typing speed here
-      return () => clearInterval(typingInterval); // Cleanup interval on unmount
+      }, 20);
+      return () => clearInterval(typingInterval);
     }
-  }, [hasAnimated, fullText]);
+  }, [hasAnimated, aboutMe]);
 
   const pageStyle = {
     marginTop: "30px",
@@ -47,6 +53,7 @@ to get a glimpse of my skills and the projects I've brought to life.`;
 
   const buttonStyle = {
     marginTop: "20px",
+    marginBottom: "20px",
     backgroundColor: theme[themeMode].custom.primary,
     color: theme[themeMode].custom.text,
   };
@@ -54,7 +61,19 @@ to get a glimpse of my skills and the projects I've brought to life.`;
   return (
     <Container style={pageStyle}>
       <Grid container xs={12} columnGap={6}>
-        <Grid item xs={6}>
+        <Grid item xs={12} sm={12} md={5.1} lg={5.1} sx={{ display: { xs: "block", sm: "block", md: "none", lg:"none" } }}>
+          <img
+            src={suimage}
+            alt="Portfolio"
+            style={{
+              width: "100%",
+              height: "auto",
+              display: "block",
+              margin: "0 auto",
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
           <Typography variant="h4" gutterBottom>
             Welcome to{" "}
             <span style={{ color: "yellow", fontWeight: 600 }}>
@@ -62,13 +81,13 @@ to get a glimpse of my skills and the projects I've brought to life.`;
             </span>
           </Typography>
           <Typography variant="body1" paragraph textAlign={"justify"}>
-            {typedText || "Loading..."} {/* Display the animated text */}
+            {typedText || "Loading..."}
           </Typography>
           <Button variant="contained" style={buttonStyle}>
             View Projects
           </Button>
         </Grid>
-        <Grid item xs={5.1}>
+        <Grid item xs={12} sm={12} md={5.1} lg={5.1} sx={{ display: { xs: "none", sm: "none", md: "block", lg:"block" } }}>
           <img
             src={suimage}
             alt="Portfolio"
